30600fa8da11d91615a0e02588ca45f609573538
izidormaklary <maklaryizidor@gmail.com>
2021-09-13 10:42:15 +0200

    updated script, centered perspective

--- script.js
+++ script.js
@@ -5,91 +5,88 @@ let i = 0;
 body.addEventListener('click', e => {
     const f = i % 2 !== 0 ? 0 : 360;
     document.documentElement.style.setProperty('--rotation', `${f}deg`)
-    for (let row of rows1) {
-        if (i % 2 === 0) {
-            clickAnimation(rows1)
-            clickAnimation(rows2)
-            break;
+    clickAnimation(rows1)
+    clickAnimation(rows2)
+    if (i % 4 === 0) {
+        for (let x = 0; x < 10; x++) {
+            if (i % 2 === 0) {
+                setTimeout(() => {
+                    clickAnimation(rows1)
+                    clickAnimation(rows2)
+                }, x * 300)
+            }
+            i++;
         }
-        let delay = rows1.indexOf(row);
-        setTimeout(() => {
-            clickAnimation(rows1)
-        }, delay * 250)
-        setTimeout(() => {
-            clickAnimation(rows2)
-        }, delay * 250)
     }
+
     i++;
 })
 window.addEventListener('scroll', () => {
     if (window.scrollY % 30 === 0) {
         const rotation = i % 2 !== 0 ? 0 : 360;
         document.documentElement.style.setProperty('--rotation', `${rotation}deg`)
-        for (let row of rows1) {
+        clickAnimation(rows1)
+        clickAnimation(rows2)
+        for (let x = 0; x < 10; x++) {
             if (i % 2 === 0) {
-                clickAnimation(rows1)
-                clickAnimation(rows2)
-                break;
-            }else {
-                let delay = Array.from(rows1).indexOf(row);
                 setTimeout(() => {
                     clickAnimation(rows1)
-                }, delay * 400)
-                setTimeout(() => {
                     clickAnimation(rows2)
-                }, delay * 400)
+                }, x * 300)
             }
+            i++;
         }
-
-        i++;
     }
 })
 
 
 function clickAnimation(rows) {
+    let random = Math.floor(Math.random() * rows.length);
     let r = Math.floor(Math.random() * 360);
     let color = `hsl(${r},${100}%,${70}% )`
-    if (Math.floor(Math.random()*10)%5===0) {
+    if (Math.floor(Math.random() * 10) % 5 === 0) {
 //Math.floor(Math.random()*10)%5===0#
-        let offset = Math.floor(Math.random()*20)
-        const rowPart= Math.round(Math.random())? rows1.slice(offset,10+offset) :rows2.slice(offset,10+offset);
+        let offset = Math.floor(Math.random() * 20)
+        const rowPart = Math.round(Math.random()) ? rows1.slice(offset, 10 + offset) : rows2.slice(offset, 10 + offset);
         let rowCounter = 0
         let additionalDelay = 8;
         for (let row of rowPart) {
             let cubeCounter = 0;
             for (let cube of Array.from(row.children).reverse()) {
-                if (cubeCounter%2===0){
-                paintCube(cube, (cubeCounter+additionalDelay*2) * 40,  (cubeCounter+additionalDelay) * 70, color)
+                 cube.style.borderRadius = `${50}% ${0}% ${0}% ${50}%`
+                if (cubeCounter % 2 === 0 && rowCounter % 2 === 0) {
+
+                    paintCube(cube, (cubeCounter + additionalDelay * 2) * 10, (cubeCounter + additionalDelay) * 35, color)
+                } else if (cubeCounter % 2 !== 0 && rowCounter % 2 !== 0) {
+                    paintCube(cube, (cubeCounter + additionalDelay * 2) * 10, (cubeCounter + additionalDelay) * 35, color)
                 }
                 cubeCounter++;
             }
-            console.log(additionalDelay)
-            if(rowCounter<4){
+            if (rowCounter < 4) {
                 additionalDelay--
-            }else{
-                additionalDelay=rowCounter
+            } else {
+                additionalDelay = rowCounter
             }
             rowCounter++;
         }
     } else {
-
         let rowArrLength = rows.length - 1;
-        let random = Math.floor(Math.random() * rowArrLength);
-        let mainRow = random;
-        let before = random === 0 ? rows.length - 1
-            : random - 1;
-        let after = random === rows.length - 1 ? 0
-            : random + 1;
 
+        let mainRow = random;
+        let before = random === 0 ? rowArrLength - 1
+            : random - 1;
+        let after = random === rowArrLength - 1 ? 0
+            : random + 1;
         let cubeArrCenter = Array.from(rows[mainRow].children).reverse();
         let cubeArrBefore = Array.from(rows[before].children).reverse();
         let cubeArrAfter = Array.from(rows[after].children).reverse();
         for (let i = 0; i < 100; i++) {
+            color = `hsl(${r},${100}%,${100 - i / 2}% )`
             let cube = cubeArrCenter[i];
             let beforeCube = cubeArrBefore[i];
             let afterCube = cubeArrAfter[i];
-            paintCube(afterCube, (i + 3) * 25, (100 + i) * 20, color)
-            paintCube(cube, i * 20, (80 + i) * 20, color)
+            paintCube(afterCube, (i + 3) * 20, (80 + i) * 15, color)
+            paintCube(cube, i * 10, (50 + i) * 20, color)
             paintCube(beforeCube, (i + 3) * 25, (100 + i) * 20, color)
         }
     }
@@ -97,10 +94,10 @@ function clickAnimation(rows) {
 
 function paintCube(cube, delay1, delay2, color) {
     setTimeout(() => {
-        //beforeCube.style.borderRadius = `${i}% ${0}% ${0}% ${i}%`
         cube.style.backgroundColor = color;
     }, delay1)
     setTimeout(() => {
+         cube.style.borderRadius = "10%"
         cube.style.backgroundColor = 'transparent';
     }, delay2)
 }
